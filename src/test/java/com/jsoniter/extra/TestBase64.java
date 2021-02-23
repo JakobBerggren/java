b93c594fa3acf7a21bffa54cbdb8ccc2b8f3f281
unknown <ntomsic@kth.se>
2021-02-23 21:35:42 +0100

    Added tests for base64 to improve coverage

--- src/test/java/com/jsoniter/extra/TestBase64.java
+++ src/test/java/com/jsoniter/extra/TestBase64.java
@@ -24,4 +24,12 @@ public class TestBase64 extends TestCase {
     public void test_illegal_front() {
         assertEquals("abc", new String(JsonIterator.deserialize("\"@YWJj\"", byte[].class)));
     }
+    
+    public void test_illegal_back() {
+        assertEquals("abc", new String(JsonIterator.deserialize("\"YWJj@\"", byte[].class)));
+    }
+
+    public void test_padding_back() {
+        assertEquals("abcd", new String(JsonIterator.deserialize("\"YWJjZA==\"", byte[].class)));
+    }
 }
